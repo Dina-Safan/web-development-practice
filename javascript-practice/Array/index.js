2d5254dba7154ff02639795f808b386370f657a4
dinasafan <dinasafan83@gmail.com>
2026-02-23 22:54:31 +0200

    First Elements of Array Write a JavaScript function to get the first element of an array. Passing the parameter 'n' will return the first 'n' elements of the array.

--- javascript-practice/Array/index.js
+++ javascript-practice/Array/index.js
@@ -22,3 +22,17 @@ console.log(array_Clone([1, 2, [4, 0]]));
  */
 
 //&--------------------------------------------------------
+
+//^3. First Elements of Array Write a JavaScript function to get the first element of an array. Passing the parameter 'n' will return the first 'n' elements of the array.
+
+/* function test(arr , n=1){
+    var result =[]
+    for(var i=0 ;i< n ;i++){
+        if(i+1 > arr.length) return result;
+        result.push(arr[i]);
+    }
+    return result
+}
+
+console.log(test([1,2,3,6]));
+ */
