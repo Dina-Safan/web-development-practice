9f8142f0e3d7f075c80a383c448c4febd5e7125e
dinasafan <dinasafan83@gmail.com>
2026-02-23 22:06:37 +0200

    Check Array Input is an array or not.

---
+++ javascript-practice/Array/index.js
@@ -0,0 +1,14 @@
+//^ 1. Check Array Input Write a JavaScript function to check whether an `input` is an array or not.
+
+/* function is_Array(input){
+    if(Array.isArray(input)) return "true ,this input is array "
+    else  return"false , this input is not array"
+
+}
+console.log(is_Array("guyoi"));
+console.log(is_Array([1,3]));
+
+ */
+
+//&--------------------------------------------------------
+
