9e9c8f9e84d7df5a0f06cb2696e388ef802316d6
dinasafan <dinasafan83@gmail.com>
2026-02-24 00:04:59 +0200

    List Object Properties

---
+++ javascript-practice/Object/object.js
@@ -0,0 +1,16 @@
+
+//^ 1. List Object Properties Write a JavaScript program to list the properties of a JavaScript object. 
+/* var student = {
+name : "David Rayy",
+sclass : "VI",
+rollno : 12 }; 
+ 
+for(let x in student){
+   console.log(x);
+}
+ */
+
+//&----------------------------------------------------
+
+
+
