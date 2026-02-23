f9993c34126b64d7e4e15ae5f67b46d76aaa84ed
dinasafan <dinasafan83@gmail.com>
2026-02-24 00:09:43 +0200

    Delete Property

--- javascript-practice/Object/object.js
+++ javascript-practice/Object/object.js
@@ -12,5 +12,22 @@ for(let x in student){
 
 //&----------------------------------------------------
 
+//^2. Delete Property Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
+
+/*  var student = {
+name : "David Rayy",
+sclass : "VI",
+rollno : 12 }; 
+
+console.log("Before Delete ");
+console.log(student);
+
+delete student.rollno;
+
+console.log("After Delete ");
+console.log(student); */
+
+//&--------------------------------------------------------
+
 
 
