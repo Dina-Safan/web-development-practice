82f70a2a21278ce5d03f64848073d9d94187d696
dinasafan <dinasafan83@gmail.com>
2026-02-25 03:19:23 +0200

    Check String Input Write a JavaScript function to check whether an 'input' is a string or not

---
+++ javascript-practice/String/string.js
@@ -0,0 +1,9 @@
+//^ 1. Check String Input Write a JavaScript function to check whether an 'input' is a string or not
+
+/* function is_string(input){
+    if(typeof input =="string") return true;
+    return false;
+}
+console.log(is_string([1, 2, 4, 0])); */
+
+//&----------------------------------------------
