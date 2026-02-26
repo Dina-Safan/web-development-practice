07e66d6e21903e92d71e84d4791d4c0c557a1c3c
dinasafan <dinasafan83@gmail.com>
2026-02-26 17:54:38 +0200

    Regx Variable

--- Crud Op-Task/crud.js
+++ Crud Op-Task/crud.js
@@ -1,4 +1,4 @@
-//^ Html Element
+<//^ Html Element
 
 const showAddedModalBtn=document.getElementById("showAddedModalBtn");
 const seacrhInput=document.getElementById("seacrhInput");
@@ -16,8 +16,14 @@ const AddBtn=document.getElementById("AddBtn");
 
 //?varaibles
 
+var nameRegx=/^[A-Za-z\s]{3,}$/;
+var priceRegx = /^([5-9][0-9]|[0-9][0-9][0-9]|1000)$/;
+var categoryRegx = /^[A-Za-z\s]{3,}$/;
+var sizeRegx = /^(50|[5-9][0-9]|[1-9][0-9][0-9]+)$/;
+var scentRegx=/^[A-Za-z\s]{3,}$/;
+
 
 //& Function
 
 
-//~Events
+//~Events>
