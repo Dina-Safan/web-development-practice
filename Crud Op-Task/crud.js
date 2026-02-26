45fea0470fac37de7e515a4d2d0b856d68d7d402
dinasafan <dinasafan83@gmail.com>
2026-02-26 17:25:56 +0200

    selected htnl elements

---
+++ Crud Op-Task/crud.js
@@ -0,0 +1,23 @@
+//^ Html Element
+
+const showAddedModalBtn=document.getElementById("showAddedModalBtn");
+const seacrhInput=document.getElementById("seacrhInput");
+const model=document.querySelector(".model");
+const addHeading=document.getElementById("hAdd");
+const updateHeading=document.getElementById("hUpdate");
+const productNameInput=document.getElementById("productNameInput");
+const productPriceInput=document.getElementById("productPriceInput");
+const productCategoryInput=document.getElementById("productCategoryInput");
+const productSizeInput=document.getElementById("productSizeInput");
+const productScentInput=document.getElementById("productScentInput");
+const updateBtn=document.getElementById("updateBtn");
+const AddBtn=document.getElementById("AddBtn");
+
+
+//?varaibles
+
+
+//& Function
+
+
+//~Events
