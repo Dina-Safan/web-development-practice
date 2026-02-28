e592201ed87b97ab8cd1037eaa0dd2d5f95fbfab
dinasafan <dinasafan83@gmail.com>
2026-02-28 20:36:18 +0200

    Print Nested Array Elements

--- javascript-practice/Array/array.js
+++ javascript-practice/Array/array.js
@@ -184,9 +184,19 @@ console.log(dashesBetweenEvens(num)); */
  
  
  //&------------------------------------------------------
- 
- 
 
+ //^10. Print Nested Array Elements Write a JavaScript program that prints the elements of the following array.
+
+/*  var arr = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+ for(var i=0;i<arr.length; i++){
+    console.log(`Row ${i+1}`);
+    
+   for(var j=0;j<arr[i].length;j++){
+    console.log(arr[i][j]);
+   }
+   
+ }
+ */
 
  
  
