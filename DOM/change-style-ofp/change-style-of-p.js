3126ca686e6eb3a24b132465ecdd7ea497796d28
dinasafan <dinasafan83@gmail.com>
2026-03-02 23:54:01 +0200

    Style Paragraph Clicking on the button the font, font size, and color of the paragraph text will be changed.

---
+++ DOM/change-style-ofp/change-style-of-p.js
@@ -0,0 +1,10 @@
+//^ 1. Style Paragraph Clicking on the button the font, font size, and color of the paragraph text will be changed.
+
+const text=document.getElementById("text");
+
+function js_style(){
+    text.style.fontSize="40px";
+    text.style.color="green";
+    text.style.fontFamily="Georgia";
+    text.style.fontStyle="italic"
+}
